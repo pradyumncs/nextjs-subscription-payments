3b2c7dad53dbf73acf5547ff36670440f6d76991
pradyumncs <pradyumncs@gmail.com>
2024-09-08 16:40:43 +0530

    sa

--- dashboard/components/admin-panel/sidebar.tsx
+++ dashboard/components/admin-panel/sidebar.tsx
@@ -15,11 +15,13 @@ export function Sidebar() {
 
   return (
     <aside
-      className={cn(
-        "bg-white lg:bg-[#EAEEFE] fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
-      )}
-    >
+    className={cn(
+      "bg-black text-white fixed top-0 left-0 z-20 h-screen transition-all duration-300 ease-in-out",
+      sidebar?.isOpen
+        ? "w-72 lg:translate-x-0"
+        : "w-[90px] lg:translate-x-0 -translate-x-full"
+    )}
+  >
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
       <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
         <Button
